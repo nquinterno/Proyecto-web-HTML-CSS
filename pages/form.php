7425dd54eeb5028760bce15ba72b01722b5d2cb9
Nicolas Quinterno <nquinterno@gmail.com>
2022-07-17 18:43:40 -0300

    Version 5.2 Web Divididos

--- pages/form.php
+++ pages/form.php
@@ -4,20 +4,18 @@ $mail = $_POST['mail'];
 $destinatario = $_POST['destinatario'];
 $comentarios = $_POST['comentarios'];
 
-$header .= "Content-Type text/plain";
+$header = "Content-Type: text/plain";
 
 $mensaje = "Mensaje Enviado por" . $nombre . ",\r\n";
-$mensaje = "Su mail es" . $mail .",\r\n";
-$mensaje = "Dirigido a" . $destinatario . ",\r\n";
-$mensaje = "Su mensaje:" . $comentarios;
+$mensaje .= "Su mail es" . $mail .",\r\n";
+$mensaje .= "Dirigido a" . $destinatario . ",\r\n";
+$mensaje .= "Su mensaje:" . $comentarios;
 // $mensjae = "Enviado el día" date('d/m/y', time());
 
 $para = 'nquinterno@gmail.com';
 $asunto = 'Mensaje Web Divididos';
 
-mail($para, $asunto, utf8_decode($comentarios), $header);
+mail($para, $asunto, utf8_decode($mensaje), $header);
 
 header('location:contacto.html');
-
-// .../pages/contacto.html
 ?>
