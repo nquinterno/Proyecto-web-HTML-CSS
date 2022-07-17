48bd0631922cc6d5e13436dbca732e1945b41842
Nicolas Quinterno <nquinterno@gmail.com>
2022-07-17 18:25:29 -0300

    Version 5  Web Divididos

---
+++ pages/form.php
@@ -0,0 +1,23 @@
+<?php
+$nombre = $_POST['nombre'];
+$mail = $_POST['mail'];
+$destinatario = $_POST['destinatario'];
+$comentarios = $_POST['comentarios'];
+
+$header .= "Content-Type text/plain";
+
+$mensaje = "Mensaje Enviado por" . $nombre . ",\r\n";
+$mensaje = "Su mail es" . $mail .",\r\n";
+$mensaje = "Dirigido a" . $destinatario . ",\r\n";
+$mensaje = "Su mensaje:" . $comentarios;
+// $mensjae = "Enviado el día" date('d/m/y', time());
+
+$para = 'nquinterno@gmail.com';
+$asunto = 'Mensaje Web Divididos';
+
+mail($para, $asunto, utf8_decode($comentarios), $header);
+
+header('location:index.html');
+
+// .../pages/contacto.html
+?>
